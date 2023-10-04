711cdf6fa6f945826f3877453e310a74a5a7f35b
sun88110 <138852390+sun88110@users.noreply.github.com>
2023-10-04 14:45:26 +0900

    Update Block.cs

--- crazy/Assets/Script/Block.cs
+++ crazy/Assets/Script/Block.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,40 +8,49 @@ public class Block : MonoBehaviour
     public GameObject player;
     Player playerLogic;
     Animator anim;
-    
+
 
     public float nextPushTime; // 블럭 밀기 최대(설정) 쿨타임
     public float curPushTime; // 블럭 밀기 현재(충전) 쿨타임
 
-    void Awake() {
+    void Awake()
+    {
         playerLogic = player.GetComponent<Player>();
         anim = GetComponent<Animator>();
     }
 
-    void Hit() {
+    void Hit()
+    {
         gameObject.SetActive(false);
         anim.SetBool("Hit", false);
+        Debug.Log("진욱이 왔다감");
     }
 
-    void OnTriggerEnter2D(Collider2D  obj) {
-        if(obj.gameObject.tag == "upWater" || obj.gameObject.tag == "downWater" || obj.gameObject.tag == "leftWater" || obj.gameObject.tag == "rightWater") {
+    void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (obj.gameObject.tag == "upWater" || obj.gameObject.tag == "downWater" || obj.gameObject.tag == "leftWater" || obj.gameObject.tag == "rightWater")
+        {
             anim.SetBool("Hit", true);
             Invoke("Hit", 0.2f);
         }
     }
 
-    void OnCollisionStay2D(Collision2D obj) {
-        if(obj.gameObject.tag == "Player") {
+    void OnCollisionStay2D(Collision2D obj)
+    {
+        if (obj.gameObject.tag == "Player")
+        {
             curPushTime += Time.deltaTime;
 
-            if(curPushTime > nextPushTime) {
+            if (curPushTime > nextPushTime)
+            {
 
                 curPushTime = 0;
             }
         }
     }
 
-    void OnCollisionExit2D(Collision2D other) {
+    void OnCollisionExit2D(Collision2D other)
+    {
         curPushTime = 0;
     }
 
